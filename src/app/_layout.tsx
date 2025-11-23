1692a2253b52577b2f612a1b0eb476dbc6559eef
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-23 20:16:16 -0300

    Layout

---
+++ src/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { colors } from "@/theme/colors";
+import { Stack } from "expo-router";
+
+export default function Layout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: colors.white },
+      }}
+    />
+  );
+}
