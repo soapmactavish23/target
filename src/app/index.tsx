074e222294c55c761ad2cb609c9aa48691608ef0
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-26 22:11:37 -0300

    LinearGradient

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,10 @@
-import { fontFamily } from "@/theme/fontFamily";
-import { router } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { HomeHeader } from "@/components/HomeHeader";
+import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text style={{ fontFamily: fontFamily.bold }}>Expo Router</Text>
-
-      <Button title="Nova Meta" onPress={() => router.navigate("/target")} />
-      <Button
-        title="Transação"
-        onPress={() => router.navigate("/transaction/123")}
-      />
-      <Button
-        title="Progresso"
-        onPress={() => router.navigate("/in-progress/123")}
-      />
+    <View style={{ flex: 1 }}>
+      <HomeHeader />
     </View>
   );
 }
