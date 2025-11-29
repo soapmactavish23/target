0411d1f9069d794b1c480d3e6514470bfe6d7462
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-29 00:10:07 -0300

    Componente Separator

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { View } from "react-native";
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
-      <HomeHeader />
+      <HomeHeader data={{total: 'R$ 2.000,00'}} />
     </View>
   );
 }
--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -2,8 +2,17 @@ import { Text, View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { styles } from "./styles";
 import { colors } from "@/theme/colors";
+import Separator from "../Separator";
 
-export function HomeHeader() {
+export type HomeHeaderProps = {
+  total: string;
+};
+
+type Props = {
+  data: HomeHeaderProps;
+};
+
+export function HomeHeader({ data }: Props) {
   return (
     <LinearGradient
       colors={[colors.blue[500], colors.blue[800]]}
@@ -11,7 +20,10 @@ export function HomeHeader() {
     >
       <View>
         <Text style={styles.label}>Total que você possui</Text>
+        <Text style={styles.total}>{data.total}</Text>
       </View>
+
+      <Separator color={colors.blue[400]} />
     </LinearGradient>
   );
 }
