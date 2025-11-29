00eac1edb63a8d13fc68462acb685945cd8991a4
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-29 00:24:31 -0300

    Componente Summary

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -3,6 +3,7 @@ import { LinearGradient } from "expo-linear-gradient";
 import { styles } from "./styles";
 import { colors } from "@/theme/colors";
 import Separator from "../Separator";
+import { Summary } from "../Summary";
 
 export type HomeHeaderProps = {
   total: string;
@@ -24,6 +25,18 @@ export function HomeHeader({ data }: Props) {
       </View>
 
       <Separator color={colors.blue[400]} />
+
+      <View style={styles.summary}>
+        <Summary
+          data={{ label: "Entradas", value: "R$ 6.184,90" }}
+          icon={{ name: "arrow-upward", color: colors.green[500] }}
+        />
+        <Summary
+          data={{ label: "Saídas", value: "-R$ 184,90" }}
+          icon={{ name: "arrow-downward", color: colors.red[400] }}
+          isLeft={true}
+        />
+      </View>
     </LinearGradient>
   );
 }
