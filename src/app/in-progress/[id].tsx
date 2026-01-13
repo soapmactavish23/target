5382231014d3483a4deca075c75f2bae6d6b4664
soapmactavish23 <ricknogueira1231@gmail.com>
2026-01-12 22:01:07 -0300

    Componente Transaction

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,13 +1,32 @@
+import { List } from "@/components/List";
 import { PageHeader } from "@/components/PageHeader";
 import { Progress } from "@/components/Progress";
+import { Transaction, TransactionProps } from "@/components/Transaction";
+import { TransactionTypes } from "@/utils/TransactionTypes";
 import { router, useLocalSearchParams } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { Button, DevSettings, Text, View } from "react-native";
 
 const details = {
   current: "R$ 580,00",
   target: "R$ 1.790,00",
   percentage: 25,
 };
+
+const transactions: TransactionProps[] = [
+  {
+    id: "1",
+    value: "R$ 20,00",
+    date: "12/04/2025",
+    type: TransactionTypes.Output,
+  },
+  {
+    id: "2",
+    value: "R$ 300,00",
+    date: "12/04/2025",
+    description: "CDB de 110% no banco XPTO",
+    type: TransactionTypes.Input,
+  },
+];
 
 export default function InProgress() {
   const params = useLocalSearchParams<{ id: string }>();
@@ -23,6 +42,14 @@ export default function InProgress() {
       />
 
       <Progress data={details} />
+
+      <List
+        title="Transações"
+        data={transactions}
+        renderItem={({ item }) => (
+          <Transaction data={item} onRemove={() => {}} />
+        )}
+      />
     </View>
   );
 }
