4e52a5785d93fdb7fde4f7b9c19458bf7dacf13d
soapmactavish23 <ricknogueira1231@gmail.com>
2026-01-20 21:50:22 -0300

    Componente TransactionType

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import { Button } from "@/components/Button";
 import { CurrencyInput } from "@/components/CurrencyInput";
 import { Input } from "@/components/Input";
 import { PageHeader } from "@/components/PageHeader";
+import { TransactionType } from "@/components/TransactionType";
 import { useLocalSearchParams } from "expo-router";
 import { View } from "react-native";
+import { TransactionTypes } from "@/utils/TransactionTypes";
 
 export default function Transaction() {
+  const [type, setType] = useState(TransactionTypes.Input);
   const params = useLocalSearchParams<{ id: string }>();
 
   return (
@@ -16,6 +20,7 @@ export default function Transaction() {
       />
 
       <View style={{ marginTop: 32, gap: 24 }}>
+        <TransactionType selected={type} onChange={setType} />
         <CurrencyInput label="Valor (R$)" value={0} />
         <Input
           label="Motivo (opcional)"
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -23,4 +23,4 @@ export const colors = {
   },
   white: "#ffffff",
   black: "#000000",
-}
+};
