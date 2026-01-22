4eb662028aedfee797a9ea27cbd61a4ef3addf3f
soapmactavish23 <ricknogueira1231@gmail.com>
2026-01-21 21:53:43 -0300

    Criando a tabela Transactions

--- src/database/migrate.ts
+++ src/database/migrate.ts
@@ -2,16 +2,29 @@ import { SQLiteDatabase } from "expo-sqlite";
 
 export async function migrate(database: SQLiteDatabase) {
   await database.execAsync(`
-            PRAGMA foreign_key = ON;
+            PRAGMA foreign_keys = ON;
 
             CREATE TABLE IF NOT EXISTS targets (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 name TEXT NOT NULL,
                 amount FLOAT NOT NULL,
                 created_at timestamp NOT NULL DEFAULT current_timestamp,
-                updated_at timestamp NOT NULL DEFAULT current_timestamp,
+                updated_at timestamp NOT NULL DEFAULT current_timestamp
             );
 
+            CREATE TABLE IF NOT EXISTS transactions (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                target_id INTEGER NOT NULL,
+                amount FLOAT NOT NULL,
+                observation TEXT NULL,
+                created_at timestamp NOT NULL DEFAULT current_timestamp,
+                updated_at timestamp NOT NULL DEFAULT current_timestamp,
+                
+                CONSTRAINT fk_targets_trancations
+                FOREIGN KEY (target_id) REFERENCES targets(id)
+                ON DELETE CASCADE
             );
+
+
         `);
 }
