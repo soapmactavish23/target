4276960eef69b33b0cf306ea55971f74020498d4
soapmactavish23 <ricknogueira1231@gmail.com>
2026-01-25 15:56:18 -0300

    Criando o método de create

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/Button";
 import { CurrencyInput } from "@/components/CurrencyInput";
 import { Input } from "@/components/Input";
 import { PageHeader } from "@/components/PageHeader";
+import { useTargetDatabase } from "@/database/useTargetDatabase";
 import { router, useLocalSearchParams } from "expo-router";
 import { useState } from "react";
 import { Alert, View } from "react-native";
@@ -12,6 +13,7 @@ export default function Target() {
   const [amount, setAmount] = useState(0);
 
   const params = useLocalSearchParams<{ id?: string }>();
+  const targetDatabase = useTargetDatabase();
 
   function handleSave() {
     if (!name.trim() || amount <= 0) {
@@ -32,6 +34,7 @@ export default function Target() {
 
   async function create() {
     try {
+      await targetDatabase.create({ name, amount });
       Alert.alert("Nova Meta", "Meta criada com sucesso!", [
         {
           text: "Ok",
